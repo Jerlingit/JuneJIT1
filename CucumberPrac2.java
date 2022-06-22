a9963329c4a4ada4c441da0984eac0eef178d5f2
jerlingit <jerlinthangam24@gmail.com>
2022-06-22 19:48:28 +0530

    mastergit

--- CucumberPrac2.java
+++ CucumberPrac2.java
@@ -25,6 +25,8 @@ public class CucumberPrac2 extends BaseClass {
 		Thread.sleep(3000);
 	    driver.findElement(By.xpath("//button[@type=\"submit\"]")).click();
 	    System.out.println("Search was successfull");
+	    System.out.println("test1");
+	    System.out.println("test2");
 	}
 
 	
